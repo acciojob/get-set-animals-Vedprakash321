0a8e598bb98ccfb789b0233ef5a2b3325575e5fc
Vedprakash321 <131158289+Vedprakash321@users.noreply.github.com>
2023-10-12 17:48:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
 //complete this code
-class Animal {}
+class Animal {
+	 constructor(species) {
+        this._species = species;
+    }
 
-class Dog extends Animal {}
+    get species() {
+        return this._species;
+    }
 
-class Cat extends Animal {}
+    makeSound(sound) {
+        console.log(sound);
+    }
+
+    // set age(age) {
+    //     this._age = age;
+    // }
+}
+
+class Dog extends Animal {
+	 constructor(species) {
+        super(species);
+    }
+
+    bark() {
+        console.log(`woof`);
+    }
+}
+
+class Cat extends Animal {
+	constructor(species) {
+	    super(species);
+    }
+
+   purr() {
+        console.log(`purr`);
+    }
+ }
+
+const cat = new Cat('Cat');
+console.log(cat.species); // Outputs: Cat
+cat.purr(); // Outputs: purr
+
+const dog = new Dog('Dog');
+console.log(dog.species); // Outputs: Dog
+dog.bark(); // Outputs: woof
+
 
 // Do not change the code below this line
 window.Animal = Animal;
